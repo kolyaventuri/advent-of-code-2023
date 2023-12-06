5068172a75887f2e0550f0b8b067e340e78dddc2
Kolya Venturi <me@kolya.co>
2023-12-06 12:01:57 -0700

    (day5): An incomplete part 2 solution

--- day5.zig
+++ day5.zig
@@ -8,16 +8,19 @@ fn cmpByData(_: void, a: Data, b: Data) bool {
 }
 
 pub fn main() !void {
-    const file = @embedFile("inputs/day5.txt");
+    const file = @embedFile("inputs/day5.test.txt");
     var allocator = std.heap.page_allocator;
     const lines = try String.split(allocator, file, "\n");
     defer lines.deinit();
 
     var index: usize = 0;
-    var sections = [8]std.ArrayList(i64){ std.ArrayList(i64).init(allocator), std.ArrayList(i64).init(allocator), std.ArrayList(i64).init(allocator), std.ArrayList(i64).init(allocator), std.ArrayList(i64).init(allocator), std.ArrayList(i64).init(allocator), std.ArrayList(i64).init(allocator), std.ArrayList(i64).init(allocator) };
+    var sections = [7]std.ArrayList(i64){ std.ArrayList(i64).init(allocator), std.ArrayList(i64).init(allocator), std.ArrayList(i64).init(allocator), std.ArrayList(i64).init(allocator), std.ArrayList(i64).init(allocator), std.ArrayList(i64).init(allocator), std.ArrayList(i64).init(allocator) };
     for (sections) |section| {
         defer section.deinit();
     }
+
+    var seed_list = std.ArrayList([2]i64).init(allocator);
+    defer seed_list.deinit();
 
     // Parsing
     for (lines.items) |line| {
@@ -65,18 +68,26 @@ pub fn main() !void {
         }
 
         if (numbers.items.len > 0) {
+            if (index == 0) {
+                var j: usize = 0;
+                while (j < numbers.items.len) {
+                    try seed_list.append([2]i64{ numbers.items[j], numbers.items[j + 1] });
+                    j += 2;
+                }
+                continue;
+            }
             for (numbers.items) |n| {
-                try sections[index].append(n);
+                try sections[index - 1].append(n);
             }
         }
     }
 
-    var lists = [_]std.ArrayList(Data){std.ArrayList(Data).init(allocator)} ** 7;
-    for (lists) |list| {
+    var maps = [_]std.ArrayList(Data){std.ArrayList(Data).init(allocator)} ** 7;
+    for (maps) |list| {
         defer list.deinit();
     }
 
-    for (sections[1..sections.len], 0..) |section, i| {
+    for (sections, 0..) |section, i| {
         // std.debug.print("Section {d} (size {d})...\n", .{ i + 1, section.items.len });
         var j: usize = 0;
         while (j < section.items.len) {
@@ -86,47 +97,77 @@ pub fn main() !void {
 
             const data = Data{ .destination = destination, .range_start = range_start, .range_size = range_size };
 
-            try lists[i].append(data);
+            try maps[i].append(data);
 
             // std.debug.print("\tStored row {d} vs {d}\n", .{ j, section.items.len });
 
             j += 3;
         }
 
-        std.mem.sort(Data, lists[i].items, {}, cmpByData);
+        std.mem.sort(Data, maps[i].items, {}, cmpByData);
     }
     // std.debug.print("Done parsing.\n", .{});
     // End Parse
 
     // Begin logic
     var min: i64 = -1;
-    for (sections[0].items) |seed| {
-        var temp: i64 = seed;
-        // std.debug.print("Seed {d}: {d}\n", .{ seed, seed });
-        for (lists) |list| {
-            for (list.items) |data| {
-                const destination_start = data.destination;
-                const source_start = data.range_start;
-                const size = data.range_size - 1;
+    for (seed_list.items) |seed| {
+        var t_left = seed[0];
+        var t_right = t_left + seed[1] - 1;
+        std.debug.print("Seed {d} -> {d} (count: {d})\n", .{ t_left, t_right, seed[1] });
+        for (maps, 0..) |map, m| {
+            std.debug.print("Map({d}), start: t_left = {d}, t_right = {d}\n", .{ m + 1, t_left, t_right });
 
-                if (temp >= source_start and temp <= source_start + size) {
-                    // std.debug.print("{d} -> ", .{temp});
-                    // std.debug.print("(s: {d}, se: {d})", .{ source_start, source_start + size });
-                    // std.debug.print("(d: {d}, de: {d})", .{ destination_start, destination_start + size });
-                    const offset = destination_start - source_start;
-                    temp = temp + offset;
-                    break;
+            for (map.items) |data| {
+                const range_end = data.range_start + data.range_size - 1;
+                std.debug.print("Range: {d} -> {d}\n", .{ data.range_start, range_end });
+                std.debug.print("\tExpeted output range: {d} -> {d}\n", .{ data.destination, data.destination + data.range_size - 1 });
+                const move = data.destination - data.range_start;
+                std.debug.print("\tMove by {d}\n", .{move});
+                if (t_left > range_end) {
+                    continue;
                 }
+
+                if (t_right < data.range_start) {
+                    continue;
+                }
+
+                // We're within the valid start range
+                const offset_left = t_left - data.range_start;
+                var min_left: i64 = 0;
+                var max_right: i64 = 0;
+
+                if (data.range_start > t_left) {
+                    min_left = data.range_start;
+                } else {
+                    min_left = t_left;
+                }
+
+                if (range_end < t_right) {
+                    max_right = range_end;
+                } else {
+                    max_right = t_right;
+                }
+
+                std.debug.print("\tofffset_left={d}, min_left={d}, max_right={d}\n", .{ offset_left, min_left, max_right });
+
+                t_left = min_left + move;
+                t_right = max_right + move;
+                std.debug.print("\tNew t_left={d}\n", .{t_left});
+                std.debug.print("\tNew t_right={d}\n", .{t_right});
             }
-            // std.debug.print("\n", .{});
+
+            std.debug.print("\n", .{});
         }
 
-        // std.debug.print("Check {d}\n", .{temp});
+        std.debug.print("Final t_left = {d}, t_right={d}\n", .{ t_left, t_right });
         if (min == -1) {
-            min = temp;
-        } else if (temp < min) {
-            min = temp;
+            min = t_left;
+        } else if (t_left < min) {
+            min = t_left;
         }
+
+        break;
     }
 
     std.debug.print("Part 1: {d}\n", .{min});
