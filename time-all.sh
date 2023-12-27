24477052693a729b9318f9daf09ec507d2c19ad8
Kolya Venturi <me@kolya.co>
2023-12-27 14:54:50 -0700

    Robust timing script

--- time-all.sh
+++ time-all.sh
@@ -3,9 +3,14 @@
 dir="./zig-out/bin"
 files=($(ls "$dir" | xargs -n 1 basename))
 
-concatenated=""
+total=0
 for file in "${files[@]}"; do
-    concatenated+="./zig-out/bin/$file;"
+  output=$( { time ./zig-out/bin/$file; } 2>&1 )
+  real_time=$(echo "$output" | grep 'real' | awk '{gsub(/m|s/, " "); print $2 * 60 + $3}')
+
+  echo "$file: ${real_time}s"
+  total=$(echo "$total + $real_time" | bc)
 done
 
-time sh -c "$concatenated"
+echo "Total time: ${total}s"
+echo "AVG time: $(echo "scale=4; $total / ${#files[@]}" | bc)s"
